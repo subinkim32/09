10e9fbb68ae8fc95307440bf8b7bc8d9c206bf22
subinkim32 <salgeum1222@gmail.com>
2023-11-05 17:49:17 +0900

    03_2

--- main.c
+++ main.c
@@ -6,19 +6,20 @@
 
 int main(int argc, char *argv[]) {
 	int i;
-	int grade[SIZE];
-	int score[SIZE];
-		
+	int a[SIZE] = {1, 2, 3, 4, 5};
+	int b[SIZE] = {1, 2, 3, 4, 6};
+	int flag = 0;
 	
 	for (i=0; i<SIZE; i++)
 	{
-		grade[i] = rand() % 100;
-		score[i] = grade[i];
-		
-		printf("score[%d] = %d\n", i, score[i]);
+		if (a[i] != b[i])
+		{
+			printf("a and b are not the same.");
+			flag = 1;
+		}
 	}
-		
-
 	
+	if (flag == 0)
+		printf("a and b are the same.\n");
 	return 0;
 }
